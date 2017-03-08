161407ea05b387d7ae64c31fa13f36447c89476c
Ross Riley <riley.ross@gmail.com>
2017-03-08 17:25:53 +0000

    update to extend field type base

--- src/Field/BootstrapColorPickerPickerField.php
+++ src/Field/BootstrapColorPickerPickerField.php
@@ -2,9 +2,9 @@
 
 namespace sahassar\bootstrapcolorpicker\Field;
 
-use Bolt\Storage\Field\FieldInterface;
+use Bolt\Storage\Field\Type\FieldTypeBase;
 
-class BootstrapColorPickerPickerField implements FieldInterface
+class BootstrapColorPickerPickerField extends FieldTypeBase
 {
 
     public function getName()
